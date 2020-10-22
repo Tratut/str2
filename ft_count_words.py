88de58822d1ce7441b04b6e3c9e01172eee992fc
Tratut <64106392+Tratut@users.noreply.github.com>
2020-10-22 23:58:29 +0300

    Add files via upload

--- ft_count_words.py
+++ ft_count_words.py
@@ -7,24 +7,32 @@ def ft_len(st):
 
 def ft_count_words(st):
     count = 0
-    while st[0] == " ":
-        cop = ''
-        for i in range(1, ft_len(st)):
-            cop += st[i]
-        st = cop
-    while " " in st:
-        ind = st.index(" ")
-        copp = ''
-        for g in range(ind + 1, ft_len(st)):
-            copp += st[g]
-        st = copp
-        if ft_len(st) > 0:
-            while st[0] == " ":
-                cop = ''
-                for i in range(1, ft_len(st)):
-                    cop += st[i]
-                st = cop
-            count += 1
-    return count + 1
+    flag = False
+    for i in st:
+        if i != " ":
+            flag = True
+    if flag:
+        while st[0] == " ":
+            cop = ''
+            for i in range(1, ft_len(st)):
+                cop += st[i]
+            st = cop
+        while " " in st:
+            ind = st.index(" ")
+            copp = ''
+            for g in range(ind + 1, ft_len(st)):
+                copp += st[g]
+            st = copp
+            if ft_len(st) > 0:
+                while st[0] == " ":
+                    cop = ''
+                    for i in range(1, ft_len(st)):
+                        cop += st[i]
+                    st = cop
+                count += 1
+    if flag:
+        return count + 1
+    else:
+        return 0
 
 # print(ft_count_words("   f  adsd dfhsdg dsgdsfg    ewgfsgfafedga"))

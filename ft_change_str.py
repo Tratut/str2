155d924e23178ea693f690fdabe51fd52e70e1c3
Tratut <64106392+Tratut@users.noreply.github.com>
2020-10-22 21:38:27 +0300

    Add files via upload

---
+++ ft_change_str.py
@@ -0,0 +1,22 @@
+def ft_len(st):
+    kol = 0
+    for i in st:
+        kol += 1
+    return kol
+
+
+def ft_change_str(str1, str2, str3):
+    word_ln = ft_len(str1)
+    while str1 in str3:
+        ind = str3.index(str1)
+        first = ''
+        sec = ''
+        for i in range(0, ind):
+            first += str3[i]
+        for i in range(ind + word_ln, ft_len(str3)):
+            sec += str3[i]
+        str3 = first + str2 + sec
+    return str3
+
+
+# print(ft_change_str("23", "asfdsfsgf", "1234"))
